a905f74c48d2a16e9c16e30f668e713737ea8715
javkhaanj7 <javkhaanj7@gmail.com>
2014-04-16 10:48:09 -0700

    Create FirstReverse.rb

---
+++ FirstReverse.rb
@@ -0,0 +1,12 @@
+#Using the Ruby language, have the function FirstReverse(str) take the str parameter 
+#being passed and return the string in reversed order. 
+#Correct Sample Outputs:
+#Input = "coderbyte" Output = "etybredoc"
+#Input = "I Love Code" Output = "edoC evoL I"
+#http://coderbyte.com/
+ 
+def FirstReverse(str)
+ 
+  return str.reverse! 
+ 
+end
